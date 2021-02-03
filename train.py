3d581b99d47bb1e5f67fce690a49d77858914ebe
partydog <waeechang@gmail.com>
2021-02-03 11:47:27 +0800

    Update train.py

--- train.py
+++ train.py
@@ -92,6 +92,7 @@ if __name__ == '__main__':
             ############################
             # (2) Update G network: minimize 1-D(G(z)) + Perception Loss + Image Loss + TV Loss
             ###########################
+            '''
             netG.zero_grad()
             g_loss = generator_criterion(fake_out, fake_img, real_img)
             g_loss.backward()
@@ -99,6 +100,13 @@ if __name__ == '__main__':
             fake_img = netG(z)
             fake_out = netD(fake_img).mean()
             
+            optimizerG.step()
+            '''
+            netG.zero_grad()
+            fake_img = netG(z)
+            fake_out = netD(fake_img).mean()
+            g_loss = generator_criterion(fake_out, fake_img, real_img)
+            g_loss.backward()
             optimizerG.step()
 
             # loss for current batch before optimization 
